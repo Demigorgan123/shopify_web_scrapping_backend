207429a18ae26ee88ad65a48483b29200aa49fb8
Demigorgan123 <rbanswal123@gmail.com>
2024-08-07 04:29:45 +0530

    create extractProductInfo controller

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import robotsTxtParser from "../controllers/robotsTxtParser.js";
 import fetchSitemap from "../controllers/fetchSiteMap.js";
 import parseSitemap from "../controllers/parseSiteMap.js";
 import findProductSitemap from "../controllers/findProductSitemap.js";
+import extractProductInfo from "../controllers/extractProductInfo.js";
 const url = 'https://plumgoodness.com'
 const siteMapUrl = await robotsTxtParser(url);
 const siteMapXml = await fetchSitemap(siteMapUrl);
 const siteMapData = await parseSitemap(siteMapXml);
-console.log(findProductSitemap(siteMapData));
+const productSitemapUrl = await findProductSitemap(siteMapData);
+const productInfo = await extractProductInfo(productSitemapUrl);
+console.log(productInfo);
